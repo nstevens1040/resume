5b50716f43253a5f11d11cad26a2a7ebf5d6a869
Nicholas Stevens <nstevens@nanick.org>
2022-01-02 14:49:41 -0600

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,20 @@
+self.addEventListener('install', (e) => {
+  e.waitUntil(
+    caches.open('nstevens1040-store').then((cache) => cache.addAll([
+      './index.html',
+      './profile.png',
+      './favicon.ico',
+      './favicon.svg',
+      './logo_preview_.png,
+      './Nicholas Stevens - Technical Support Engineer.pdf',
+      './Nicholas Stevens - Technical Support Engineer.docx'
+    ])),
+  );
+});
+
+self.addEventListener('fetch', (e) => {
+  console.log(e.request.url);
+  e.respondWith(
+    caches.match(e.request).then((response) => response || fetch(e.request)),
+  );
+});
